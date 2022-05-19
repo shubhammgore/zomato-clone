991ce4c45e46804eec82b929b8b6ddfc72c2eb71
shubham <shubh.shubham21@gmail.com>
2022-05-19 19:02:07 +0530

    final design

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div className='absolute-center max-width'>
+			Made with by ☕ Shubham Gore
+		</div>
+	);
+};
+
+export default Footer;
